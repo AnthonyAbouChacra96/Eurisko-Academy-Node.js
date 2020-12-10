0a9af913d9bf5caee3e5216f9766f7a56372ff9b
unknown <anthonyabouchakara.aac@gmail.com>
2020-12-10 02:04:25 +0200

    feat:(Section 3)38. Using the Node Modules System

---
+++ nodejs-complete-guide/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const routes=require('./routes');
+// function rqListener(req,res){
+
+// }
+console.log(routes.someText)
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
