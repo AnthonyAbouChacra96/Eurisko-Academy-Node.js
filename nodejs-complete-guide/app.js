6f8500db40dd1f2865a75c764cab66693132146e
unknown <anthonyabouchakara.aac@gmail.com>
2020-12-13 00:19:40 +0200

    feat: (Section 5)74. Serving Files Statically

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -1,16 +1,20 @@
 const express= require('express');
+const path = require("path");
 const bodyParser=require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const rootDir = require("./Util/path"); 
 
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(rootDir,"public")));
 
-
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-
+app.use((req,res,next)=>{
+	 res.sendFile(path.join(rootDir, "Views", "404.html"));
+});
 
 app.listen(3000);
--- nodejs-complete-guide/routes/admin.js
+++ nodejs-complete-guide/routes/admin.js
@@ -1,15 +1,22 @@
 const express=require("express");
+
+const path = require("path");
+
+const rootDir=require('../Util/path'); 
+
 const router= express.Router();
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
 router.get("/add-product", (req, res, next) => {
   //console.log('In Another Middleware!');
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit" >Add Product</button></form>'
-  );
+  // res.send(
+  //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit" >Add Product</button></form>'
+	// );
+	 res.sendFile(path.join(rootDir, "Views", "add-product.html"));
+
 });
 
 module.exports =router;
